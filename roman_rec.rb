736dee8efb1d924a11021b7734d1e2df4a430d36
John Marks <johnjmarks4@gmail.com>
2017-09-18 22:29:10 -0500

    Refactor recursive version

--- roman_rec.rb
+++ roman_rec.rb
@@ -1,7 +1,11 @@
-class Translator
+class Translation
   attr_reader :num
 
-  def initialize
+  def initialize(i)
+    @num = ""
+    @remainder = 0
+    @recorder = 0
+    @original = i
     @roman = {
           1000 => "M",
           900 => "CM",
@@ -17,48 +21,33 @@ class Translator
           4 => "IV",
           1 => "I"
         }
+
+    track_progress(i)
   end
 
-  def translate_rec(i)
-    if @roman.key?(i)
-      @recorder += i
-      @roman[i]
-    else
-      @remainder += 1
-      translate_rec(i -= 1)
-    end
-  end
-
-  def to_trans(i)
+  def track_progress(i)
     until @recorder == @original
-      next_place = translate_rec(i)
-      @num += next_place
+      @num += translate(i)
       i = @remainder
       @remainder = 0
     end
   end
 
-  def call(i)
-    @num = ""
-    @remainder = 0
-    @recorder = 0
-    @original = i
-    to_trans(i)
+  def translate(i)
+    if @roman.key?(i)
+      @recorder += i
+      @roman[i]
+    else
+      @remainder += 1
+      translate(i -= 1)
+    end
   end
 end
-
-trans = Translator.new
-
-#ary = []
-#(1..int).each do |i|
-  #ary << trans.call(i)
-#end
-#print ary
 
 print "Type any number between 1 and 4999\n\n"
 loop do
   int = gets.chomp!.to_i
   next if int == 0
-  trans.call(int)
+  trans = Translation.new(int)
   puts trans.num
 end
