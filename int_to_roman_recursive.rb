72f5c841c19ad1dc7e7edf1b80a52125c4e98045
John Marks <johnjmarks4@gmail.com>
2017-09-20 16:19:20 -0500

    Combine no_object and recursive versions

--- int_to_roman_recursive.rb
+++ int_to_roman_recursive.rb
@@ -1,53 +1,37 @@
-class Translation
-  attr_reader :num
+def int_to_roman(i)
+  num = ""
+  remainder = i
+  roman = {
+      1000 => "M",
+      900 => "CM",
+      500 => "D",
+      400 => "CD",
+      100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I"
+    }
 
-  def initialize(i)
-    @num = ""
-    @remainder = 0
-    @recorder = 0
-    @original = i
-    @roman = {
-          1000 => "M",
-          900 => "CM",
-          500 => "D",
-          400 => "CD",
-          100 => "C",
-          90 => "XC",
-          50 => "L",
-          40 => "XL",
-          10 => "X",
-          9 => "IX",
-          5 => "V",
-          4 => "IV",
-          1 => "I"
-        }
-
-    track_progress(i)
+  while remainder > 0
+    i = translate(remainder, roman)
+    num += roman[i]
+    remainder -= i
   end
+  num
+end
 
-  def track_progress(i)
-    until @recorder == @original
-      @num += translate(i)
-      i = @remainder
-      @remainder = 0
-    end
-  end
-
-  def translate(i)
-    if @roman.key?(i)
-      @recorder += i
-      @roman[i]
-    else
-      @remainder += 1
-      translate(i -= 1)
-    end
+def translate(i, roman)
+  if roman.key?(i)
+    i
+  else
+    translate(i -= 1, roman)
   end
 end
 
-print "Type any number between 1 and 4999\n\n"
-loop do
-  int = gets.chomp!.to_i
-  next if int == 0
-  trans = Translation.new(int)
-  puts trans.num
-end
+input = gets.chomp!
+puts int_to_roman(input.to_i)
